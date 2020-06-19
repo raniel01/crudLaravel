47eac8009c06a1e5b664cd0976da5d24755ba6d8
Raniel Santos <ranielsnts@hotmail.com>
2020-06-19 02:45:59 +0000

    Atualizações

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -14,4 +14,41 @@ class UserController extends Controller
             'users'=>$usuarios
             ]);
     }
+
+    public function storage (Request $request)
+    {
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->save();
+
+        return redirect('usuarios');
+    }
+
+    public function edit(User $userId)
+    {
+        return view('editar', [
+            'user'=> $userId
+            ]);
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $user->name = $request->name;
+        $user->email = $request->email;
+        if(!empty($request->password))
+        {
+            $user->password = $request->password;
+        }
+        $user->save();
+
+        return redirect('usuarios');
+    }
+
+    public function delete(User $user)
+    {
+        $user->delete();
+        return redirect('usuarios');
+    }
 }
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -3,7 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+   // return view('welcome');
+   return redirect('usuarios');
 });
 
 Route::get('usuarios', 'UserController@listarUsuarios');
+
+Route::get('cadastrar',function () {
+    return view('novo');
+});
+
+Route::post('storage','UserController@storage');
+
+Route::get('editar/{userId}', 'UserController@edit');
+
+Route::put('update/{user}', 'UserController@update');
+
+Route::delete('delete/{user}', 'UserController@delete');
